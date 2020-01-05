f87b550ecc1482ab71acfe06e8803d8f99f2b15b
BryceFitzgerald <bfitzgerald@hawk.iit.edu>
2020-01-04 21:49:54 -0600

    Chnged address

--- station-node/tcpClient.py
+++ station-node/tcpClient.py
@@ -1,7 +1,7 @@
 import socket
 
 def Main():
-    host = '127.0.0.1' # host of the surver we are connecting to
+    host = '192.168.1.14' # host of the surver we are connecting to
     port = 5000 # port of server
 
     s = socket.socket() #create a new socket
